3a82a4b1908db6d4111396304973b5e4d1f4181a
Tebogo Motibana <61407596+tebogomotibane@users.noreply.github.com>
2024-03-07 11:20:17 +0200

    ADDED TEST CODE, USED STRING INTERPOLATION

--- src/main/java/oopbasics/Main.java
+++ src/main/java/oopbasics/Main.java
@@ -1,17 +1,27 @@
 package oopbasics;
 
+import oopbasics.Animals;
+import oopbasics.Dog;
+import oopbasics.Cat;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Dog dog1 = new Dog();
+        Dog dog2 = new Dog("Simba");
+//        System.out.println(dog1.eat()); //Rax eats
+//        System.out.println(dog1.sound()); //Bark
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(dog2.eat()); //Simba eats
+        System.out.println(dog2.sound()); //Bark
+
+//        Cat cat1 = new Cat();
+        Cat cat2 = new Cat("Smokey");
+//        System.out.println(cat1.eat()); //Stormy eats
+//        System.out.println(cat1.sound()); //Meow
+
+        System.out.println(cat2.eat()); //Smokey eats
+        System.out.println(cat2.sound()); //Meow
     }
 }
