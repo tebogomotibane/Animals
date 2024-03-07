bec59248465b26bc9278513c70107f2533ffaa51
Tebogo Motibana <61407596+tebogomotibane@users.noreply.github.com>
2024-03-07 09:52:25 +0200

    completed the animal class with all methods

--- src/main/java/oopbasics/Animals.java
+++ src/main/java/oopbasics/Animals.java
@@ -1,2 +1,21 @@
-package oopbasics;public class Animals {
+package oopbasics;
+
+public class Animals {
+    private final String name;
+
+    public Animals(String animalName) {
+        this.name = animalName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String sound() {
+        return "sounds...";
+    }
+
+    public String eat() {
+        return "\"{name}\"  eats";
+    }
 }
