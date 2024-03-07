1e533c975074b58961097c34716af0198bd943fa
Tebogo Motibana <61407596+tebogomotibane@users.noreply.github.com>
2024-03-07 10:07:47 +0200

    added subclass Cat constructore

--- src/main/java/oopbasics/Cat.java
+++ src/main/java/oopbasics/Cat.java
@@ -1,2 +1,8 @@
-package oopbasics;public class Cat {
+package oopbasics;
+
+import oopbasics.Animals;
+public class Cat extends Animals{
+    public Cat(String catName){
+        super(catName);
+    }
 }
